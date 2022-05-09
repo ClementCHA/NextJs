7b77c4d0841617492f97e59d214ede53db9cd1f2
Clement CHARLES <clement.charles@origins-digital.com>
2022-05-09 10:41:14 +0200

    getStaticProps & revalidate

--- pages/contact.js
+++ pages/contact.js
@@ -1,7 +1,23 @@
 import React from "react";
 
-const contact = () => {
-  return <h1> CONTACT </h1>;
+const contact = (props) => {
+  return <h1> {props.data.quotes[0].text} </h1>;
 };
 
 export default contact;
+
+export async function getStaticProps() {
+  const quote = await fetch(
+    "https://goquotes-api.herokuapp.com/api/v1/random?count=1"
+  );
+  const data = await quote.json();
+  return {
+    props: {
+      data,
+    },
+    revalidate: 20,
+    // That propriety wont refresh before 20 secondes even if we refresh the page
+  };
+}
+
+//
--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,41 @@
-import Image from "next/image";
 import Head from "next/head";
 
 import styles from "../styles/Home.module.css";
-const Home = () => {
+const Home = (props) => {
   return (
     <>
       <Head>
         <meta http-equiv="x-UA-Compatible" content="IE-edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-
-        <title>Le blog de la cuisine Basque</title>
+        <title>Titre</title>
       </Head>
-      <div className={styles.container}>Hellow NEXT JS</div>
+      <div>
+        <h1 className={styles.title}>Vocabulaire de base</h1>
+        <table className={styles.tableau}>
+          <tbody>
+            {props.array.map((el) => (
+              <tr>
+                <td>{el.en}</td>
+                <td>{el.fr}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </>
   );
 };
 
 export default Home;
+
+export async function getStaticProps() {
+  //? Data from API
+  //! Server Side rendering, we can have some API KEY here or credential, that's totally fine cause that's never gonna be return
+  const data = await import(`/data/vocabulary.json`);
+  const array = data.vocabulary;
+  return {
+    props: {
+      array,
+    },
+  };
+}
